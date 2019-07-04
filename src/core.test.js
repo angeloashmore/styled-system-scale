8e50ec72a1fb853204f05d27bdc3c64e194c5afc
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2019-07-04 11:13:19 -1000

    test: add core unit tests

---
+++ src/core.test.js
@@ -0,0 +1,49 @@
+import { scales, composeScales, interceptScales } from './core'
+
+const theme = {
+  fontSizeScales: { base: [1, 2, 3, 4] },
+  space: { base: [0, 1, 2, 3] },
+}
+
+const fontSizeScale = scales({
+  fontSizeScale: { systemProp: 'fontSize', scale: 'fontSizeScales' },
+})
+
+const mScale = scales({ mScale: { systemProp: 'm', scale: 'space' } })
+
+describe('scales', () => {
+  test('returns parser', () => {
+    expect(typeof fontSizeScale).toBe('function')
+
+    const props = { theme, fontSizeScale: 'base' }
+    const result = fontSizeScale(props)
+    expect(result).toEqual({ ...props, fontSize: theme.fontSizeScales.base })
+  })
+})
+
+describe('composeScales', () => {
+  test('combines parsers into one', () => {
+    const parser = composeScales(fontSizeScale, mScale)
+    const props = { theme, fontSizeScale: 'base', mScale: 'base' }
+    const result = parser(props)
+    expect(result).toEqual({
+      ...props,
+      fontSize: theme.fontSizeScales.base,
+      m: theme.space.base,
+    })
+  })
+})
+
+describe('interceptScales', () => {
+  test('transforms scale prop values', () => {
+    const systemMock = jest.fn()
+
+    const props = { theme, fontSizeScale: 'base' }
+    interceptScales(fontSizeScale)(systemMock)(props)
+
+    expect(systemMock).toHaveBeenCalledWith({
+      ...props,
+      fontSize: theme.fontSizeScales.base,
+    })
+  })
+})
