afbc5758560ed5fd0f3f23951dcd2c3e535340ac
Angelo Ashmore <angelo.ashmore@walltowall.com>
2019-07-08 13:32:35 -1000

    chore: add jest support to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,20 @@
 module.exports = {
-  plugins: ['react'],
+  plugins: ['react', 'jest'],
+  extends: ['plugin:jest/recommended'],
   rules: {
     'no-var': 'warn',
     'no-unused-vars': 'warn',
-    'no-unused-vars': 'warn',
+    'no-undef': 'error',
 
     // react plugin - options
     'react/jsx-uses-react': 'warn',
     'react/jsx-uses-vars': 'warn',
     'react/jsx-key': 'warn',
     'react/jsx-no-undef': 'error',
+  },
+  env: {
+    node: true,
+    browser: true,
   },
   parser: 'babel-eslint',
   parserOptions: {
