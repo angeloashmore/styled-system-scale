9f72dc46412e1aabf8d1c5ba615d2f2610b11bf0
Angelo Ashmore <angelo.ashmore@walltowall.com>
2019-07-26 13:36:47 -1000

    test: update benchmark with more complex example

--- benchmark/test.js
+++ benchmark/test.js
@@ -9,22 +9,38 @@ const theme = {
     small: ['0rem', '1rem', '2rem', '3rem'],
     large: ['10rem', '11rem', '12rem', '13rem'],
   },
+  fontSizeScales: {
+    large: ['1rem', '2rem', '3rem', '4rem'],
+  },
+}
+
+const publishedInterceptor = published.interceptScales(
+  published.composeScales(published.spaceScales, published.typographyScales),
+)(p => p)
+const masterInterceptor = master.interceptScales(
+  master.composeScales(master.spaceScales, master.typographyScales),
+)(p => p)
+const propsWithScales = {
+  theme,
+  mScale: ['small', null, 'large'],
+  pxScale: 'large',
+  pyScale: '-large',
+  fontSizeScale: 'large',
+}
+const propsWithoutScales = {
+  theme,
+  color: 'red',
+  px: [0, 1, 2, 3],
 }
 
 const tests = [
   {
     name: 'space',
     libs: {
-      published: published.spaceScales,
-      master: master.spaceScales,
+      published: publishedInterceptor,
+      master: masterInterceptor,
     },
-    run: fn => () =>
-      fn({
-        theme,
-        mScale: ['small', null, 'large'],
-        pxScale: 'large',
-        pyScale: '-large',
-      }),
+    run: fn => () => [fn(propsWithoutScales), fn(propsWithScales)],
   },
 ]
 
